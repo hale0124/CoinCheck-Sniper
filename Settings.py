f8f91cc124c40ab83703a21f968ce2ed5e7fa1a3
Ubuntu <ubuntu@ip-172-31-0-213.ap-northeast-1.compute.internal>
2017-05-27 05:28:25 +0000

    okay now

--- Settings.py
+++ Settings.py
@@ -1,8 +1,8 @@
 url = 'wss://ws-api.coincheck.com/'
-minimum_yen_spread = 100
-increment = 20 #10
+minimum_yen_spread = 1
+increment = 10 #10
 access_token = 'LRM_Pw0_ZNLRW3ie'
 access_key = 'y89yuL_VNndfKIfnP_jM2qYjl41zsDwx'
-bitcoin = 0.05
+bitcoin = 0.1
 max_position = 0.02
 order_size = 0.01
--- market_maker.py
+++ market_maker.py
@@ -5,16 +5,21 @@ from coincheck import order,market
 order_api = order.Order(access_key=Settings.access_token, secret_key=Settings.access_key)
 #order_api.create(rate=1, amount=0.01, order_type='buy', pair='btc_jpy')
 
+clear = time.time() + 5
+def clear_your_mind_buddy_everything_is_going_to_be_okay():
+  orders = order_api.list()
+  for order in orders['orders']:
+    order_api.cancel(str(order['id']))
+
 bid_id = ''
 ask_id = ''
 def loop():
-  global bid_id, ask_id
+  global bid_id, ask_id, clear
   start_time = time.time()
   
-  if bid_id != '':
-    order_api.cancel(bid_id)
-  if ask_id != '':
-    order_api.cancel(ask_id)
+  if time.time() > clear:
+    clear_your_mind_buddy_everything_is_going_to_be_okay()
+    clear = time.time() + 5
 
   order_book = coincheckapi.get_orderbook(count=5)
   best_bid = int(float(order_book['bids'][0][0]))
@@ -27,13 +32,13 @@ def loop():
 
   position_data = coincheckapi.accounts_balance()
   net_holdings = float(position_data['btc']) + float(position_data['btc_reserved'])
-  position = round(net_holdings - Settings.bitcoin, 3)
+  position = net_holdings - Settings.bitcoin
   non_bitcoin_balance = float(position_data['jpy']) + float(position_data['jpy_reserved']) + (position * best_bid)
   if position > 0:
     buy_amount = Settings.order_size
-    sell_amount = round(Settings.order_size + abs(position), 2)
+    sell_amount = round(Settings.order_size + abs(position) - 0.002, 3)
   elif position < 0:
-    buy_amount = round(Settings.order_size + abs(position), 2)
+    buy_amount = round(Settings.order_size + abs(position) - 0.002, 3)
     sell_amount = Settings.order_size
   else:
     buy_amount = Settings.order_size
@@ -42,19 +47,31 @@ def loop():
     buy_amount = 0
   if position <= -Settings.max_position:
     sell_amount = 0
-
-  buy_order = order_api.create(rate=algorithim_best_bid, amount=buy_amount, order_type='buy', pair='btc_jpy')
-  sell_order = order_api.create(rate=algorithim_best_ask, amount=sell_amount, order_type='sell', pair='btc_jpy')
   
-  try:
-    bid_id = str(buy_order['id'])
-  except:
-    pass
+  if buy_amount > 0:
+    buy_order = order_api.create(rate=algorithim_best_bid, amount=abs(buy_amount), order_type='buy', pair='btc_jpy')
   
-  try:
-    ask_id = str(sell_order['id'])
-  except:
-    pass
+  if sell_amount > 0:
+    sell_order = order_api.create(rate=algorithim_best_ask, amount=abs(sell_amount), order_type='sell', pair='btc_jpy')
+  
+  if bid_id != '':
+    order_api.cancel(bid_id)
+  if ask_id != '':
+    order_api.cancel(ask_id)
+  
+  if buy_amount > 0:
+    try:
+      bid_id = str(buy_order['id'])
+    except:
+      print('error', buy_order)
+      bid_id = ''
+  
+  if sell_amount > 0:
+    try:
+      ask_id = str(sell_order['id'])
+    except:
+      print('error', sell_order)
+      ask_id = ''
 
 
   print("{}@{} \t \t \t {}@{} \t \t \t {} \t \t \t {}".format(buy_amount, algorithim_best_bid, sell_amount, algorithim_best_ask, position, non_bitcoin_balance))
@@ -65,3 +82,4 @@ def loop():
 #raise SystemExit
 while True:
   loop()
+  time.sleep(0.5)
